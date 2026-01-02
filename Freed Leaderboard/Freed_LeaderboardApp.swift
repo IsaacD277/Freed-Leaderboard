4f169c2d212dc6293686f91f82367d7bec6d3844
Isaac D2 <idarr08@gmail.com>
2026-01-02 14:36:44 -0500

    Initial Commit

---
+++ Freed Leaderboard/Freed_LeaderboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Freed_LeaderboardApp.swift
+//  Freed Leaderboard
+//
+//  Created by Isaac D2 on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Freed_LeaderboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
