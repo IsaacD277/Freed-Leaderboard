6c18a386faff22e8dc5767c0a7882075a5c1a2ca
Isaac D2 <idarr08@gmail.com>
2026-01-02 19:49:28 -0500

    Initial Commit

---
+++ Freed-Leaderboard-TV/Freed_Leaderboard_TVApp.swift
@@ -0,0 +1,17 @@
+//
+//  Freed_Leaderboard_TVApp.swift
+//  Freed-Leaderboard-TV
+//
+//  Created by Isaac D2 on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Freed_Leaderboard_TVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
