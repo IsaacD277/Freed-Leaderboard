4e0bf1b61d06a3acb604dc4335d4393c0f977cba
Isaac D2 <hi@isaacd2.com>
2026-01-08 16:11:53 -0500

    Refactor LeaderboardData to manage player states

--- Freed Leaderboard/Structs/Data.swift
+++ Freed Leaderboard/Structs/Data.swift
@@ -4,44 +4,74 @@ import SwiftUI
 @Observable class LeaderboardData: Codable {
     var players: [Player]
     var runningTotal: Int
-    var currentPlayerId: UUID?
+    var currentPlayerIndex: Int?
+    var round: Int?
     
-    //    enum CodingKeys: String, CodingKey {
-    //        case players, runningTotal, currentPlayerId
-    //    }
-    //
-    //
-    //    func encode(to encoder: Encoder) throws {
-    //        var container = encoder.container(keyedBy: CodingKeys.self)
-    //
-    //        try container.encode(players, forKey: .players)
-    //        try container.encode(runningTotal, forKey: .runningTotal)
-    //        try container.encode(currentPlayerId, forKey: .currentPlayerId)
-    //    }
-    //
-    //    required init(from decoder: Decoder) throws {
-    //        print("Decoding from LeaderboardData")
-    //        let container = try decoder.container(keyedBy: CodingKeys.self)
-    //        self.runningTotal = try container.decode(Int.self, forKey: .runningTotal)
-    //        self.currentPlayerId = try container.decode(UUID.self, forKey: .currentPlayerId)
-    //        self.players = try container.decode([Player].self, forKey: .players)
-    //    }
+    init(players: [Player] = [], runningTotal: Int = 0) {
+        self.players = players
+        self.runningTotal = runningTotal
+        self.currentPlayerIndex = 0
+        self.round = 1
+    }
     
-    init() {
-        players = []
+    func addPlayer(newPlayer: Player) {
+        players.append(newPlayer)
+    }
+    func removePlayer(id: UUID) {
+        players.removeAll {$0.id == id }
+    }
+    
+    func clearRunningTotal() {
         runningTotal = 0
-        currentPlayerId = nil
     }
     
-    func delete(_ player: Player) {
-        players.removeAll { $0.id == player.id }
+    func nextPlayer() {
+        guard !players.isEmpty else { return }
+        currentPlayerIndex += 1
+        if currentPlayerIndex >= players.count {
+            currentPlayerIndex = 0
+            round += 1
+        }
     }
     
-    func add(_ player: Player) {
-        players.append(player)
+    func backPlayer() {
+        guard !players.isEmpty else { return }
+        if currentPlayerIndex == 0 {
+            if round > 1 {
+                round -= 1
+            }
+            currentPlayerIndex = players.count - 1
+        } else {
+            currentPlayerIndex -= 1
+        }
     }
     
-    //    func exists(_ player: Player) -> Bool {
-    //        players.contains(where: player)
-    //    }
+    func getCurrentPlayer() -> Player? {
+        guard !players.isEmpty else { return nil }
+        return players[currentPlayerIndex]
+    }
+    
+    func getNextPlayer() -> Player? {
+        guard !players.isEmpty else { return nil }
+        let nextIndex = (currentPlayerIndex + 1) % players.count
+        return players[nextIndex]
+    }
+    
+    func getPreviousPlayer() -> Player? {
+        guard !players.isEmpty else { return nil }
+        let prevIndex = currentPlayerIndex == 0 ? players.count - 1 : currentPlayerIndex - 1
+        return players[prevIndex]
+    }
+    
+    func getLeaderboard() -> [Player] {
+        players.sorted { lhs, rhs in
+            lhs.getScore() > rhs.getScore()
+        }
+    }
+    
+    func addPlayerScore(id: UUID, score: Int) {
+        if let index = players.firstIndex(where: { $0.id == id }) {
+            players[index].addScore(score: score)
+        }
+    }
 }
